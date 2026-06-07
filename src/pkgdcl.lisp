aa0baf4f204eae516cc4fda24722b147b5e76d4d
Steve Nunez <steve@symbolics.tech>
2026-06-08 07:46:05 +0800

    Clean up exports

--- src/pkgdcl.lisp
+++ src/pkgdcl.lisp
@@ -1,9 +1,7 @@
 ;;; -*- Mode: LISP; Base: 10; Syntax: ANSI-Common-Lisp; Package: CL-USER -*-
 ;;; Copyright (c) 2022,2023,2026 by Symbolics Pte. Ltd. All rights reserved.
 ;;; SPDX-License-identifier: MS-PL
-(in-package :org.tfeb.clc-user)
 
 (uiop:define-package "statistics"
-  (:use #:cl #:let-plus)
-  (:export #:mean
-	   #:variance))
+  (:use #:cl #:let-plus))
+
